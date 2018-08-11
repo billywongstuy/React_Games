1e0ff4f37271e947928d56b538806b5f7cb9b063
Billy Wong <bwong5@stuy.edu>
2018-08-10 23:09:11 -0700

    added apple, more snake parts and movement (too slow)

--- snake/src/Game.js
+++ snake/src/Game.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Game.css';
+import * as Deque from 'double-ended-queue';
 
 //different values: H (head), B (body), F (fruit), null (empty)
 class Board extends Component {
@@ -7,57 +8,51 @@ class Board extends Component {
         super(props);
         this.state = {
             width: this.props.width,
-            height: this.props.height,
-            data: this.props.data
+            height: this.props.height
         }
     }
-
-    /*
-    renderSquare(squareData) {
-        var className = 'square';
-        if (squareData === 'H') {
-            className += ' headcell';
-        }
-        else {
-            className += ' empty'
-        }
-
-        return (
-            <div className={className}></div>
-        );
-    }
-
-    renderRow(rowData, rowNum) {
-        return rowData.map((squareData, squareNum) => {
-
-            //change this to depend on value (the whole td part)?
-            return (
-                <td key={squareNum}>
-                    {this.renderSquare(squareData)}
-                </td>
-            )
-        });
-    }
-    */
 
     renderSquare(rowNum, squareNum) {
+        /*
         var className = 'square';
+
         for (var i = 0; i < this.props.bodyParts.length; i++) {
             if (rowNum === this.props.bodyParts[i][0] && squareNum === this.props.bodyParts[i][1]) {
                 if (i == 0) {
-                    className += ' head';
+                    this.props.bodyParts.pop(i);
+                    return (
+                        <div className="square head"></div>
+                    );
                 }
                 else {
-                    className += ' tail';
+                    return (
+                        <div className="square tail"></div>
+                    );
                 }
             }
-            else {
-                className += ' empty';
-            }
+        }
+        */
+        /*
+        if (this.state.filledSquares.contains(rowNum+','+squareNum)) {
+            return <div className={this.state.filledSquares[rowNum+','+squareNum]}>/div>
+        }
+        */
+
+        const filledSquares = {};
+        for (var i = 0; i < this.props.bodyParts.length; i++) {
+            filledSquares[this.props.bodyParts[i].join(',')] = (i === 0) ? 'square head' : 'square tail';
+        }
+        if (this.props.appleLocation) {
+            filledSquares[this.props.appleLocation.join(',')] = 'square apple';
+        }
+
+        const key = rowNum + ',' + squareNum;
+        if (filledSquares.hasOwnProperty(key)) {
+            return <div className={filledSquares[key]}></div>
         }
 
         return (
-            <div className={className}></div>
+            <div className="square"></div>
         );
     }
 
@@ -75,22 +70,6 @@ class Board extends Component {
 
 
     render() {
-        /*
-        const data = this.state.data.slice();
-
-        //Should I change how it's rendered?
-        //Currently, it's based on data, which I have to update all the time
-        //Maybe just render each square normally, then add classes to each of the head and body?
-
-        const tablebody = data.map((rowData, rowNum) => {
-            return (
-                <tr key={rowNum}>
-                    {this.renderRow(rowData, rowNum)}
-                </tr>
-            )
-        });
-        */
-
         const tablebody = ((arr) => {
             for (var i = 0; i < this.props.height; i++) {
                 arr.push(
@@ -101,8 +80,6 @@ class Board extends Component {
             }
             return arr;
         })([]);
-
-        console.log(tablebody);
 
         return (
             <table>
@@ -121,30 +98,24 @@ class Game extends Component {
         super(props);
 
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.runTurn = this.runTurn.bind(this);
+        this.generateApple = this.generateApple.bind(this);
         this.moveSnake = this.moveSnake.bind(this);
+        this.handleInteractions = this.handleInteractions.bind(this);
 
-        const dataArray = new Array(this.props.height);
-        for (var i = 0; i < this.props.height; i++) {
-            dataArray[i] = new Array(this.props.width).fill(null);
-        }
-        const intervalId = setInterval(this.moveSnake, 500);
+        const intervalId = setInterval(this.runTurn, 500);
         this.state = {
-            data: dataArray,
             isPlaying: false,
-            head: [5,5], //eventually get of this anf only store in bodyparts
-            bodyParts: [[5,5]],
+            bodyParts: [[5,5], [5,4], [5,3]],
             direction: null,
+            appleLocation: null,
             intervalId: intervalId
         };
-        this.state.data[5][5] = 'H';
+        console.log(this.state);
     }
 
     handleKeyDown(event) {
-        console.log(event.key);
         if (event.key === 'ArrowUp' || event.key === 'ArrowDown' || event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-
-            console.log("directional " + this.state.direction);
-
             const directions = {'ArrowUp': 'U', 'ArrowDown': 'D', 'ArrowLeft': 'L', 'ArrowRight': 'R'};
             this.setState(
                 {
@@ -152,36 +123,46 @@ class Game extends Component {
                     isPlaying: true
                 }
             )
-
         }
-        /*
-        switch(event.key) {
-            case 'ArrowUp':
-                console.log('UP');
-                break;
-            case 'ArrowDown':
-                console.log('DOWN');
-                break;
-            case 'ArrowLeft':
-                break;
-            case 'ArrowRight':
-                break;
-            default:
-                break;
-        }
-        */
     }
 
+    runTurn() {
+        this.generateApple(); //find better way should not have to constantly make it run
+        if (this.state.isPlaying) {
+            this.moveSnake();
+            this.handleInteractions();
+        }
+    }
+
+    generateApple() {
+        if (this.state.appleLocation === null) {
+            console.log("generateApple");
+            const openSpaces = [];
+            //get all the open spaces
+            const filledSquares = {};
+            var i;
+            for (i = 0; i < this.state.bodyParts.length; i++) {
+                filledSquares[this.state.bodyParts[i].join(',')] = true;
+            }
+            for (i = 0; i < this.props.height; i++) {
+                for (var j = 0; j < this.props.width; j++) {
+                    if (!filledSquares.hasOwnProperty(i + ',' + j)) {
+                        openSpaces.push(i + ',' + j);
+                    }
+                }
+            }
+
+            const chosenAppleLocation = openSpaces[Math.floor(Math.random()*openSpaces.length)].split(',').map(Number);
+            this.setState({appleLocation: chosenAppleLocation});
+        }
+    }
 
     moveSnake() {
         if (this.state.isPlaying) {
-            const dataCopy = this.state.data.slice();
             const bodyParts = this.state.bodyParts.slice();
             const head = bodyParts[0];
 
-            //need to check for edge cases, aka game over
-            //also check for eating itself later
-            if (head[0] <= 0 || head[0] >= this.props.height || head[1] <= 0 || head[1] >= this.props.width) {
+            if (head[0] < 0 || head[0] >= this.props.height || head[1] < 0 || head[1] >= this.props.width) {
                 this.setState({
                     isPlaying: false,
                     direction: null
@@ -189,37 +170,50 @@ class Game extends Component {
                 alert('Game over!');
             }
 
-
-            dataCopy[head[0]][head[1]] = null;
             if (this.state.direction === 'U') {
-                head[0] -= 1;
-                dataCopy[head[0]][head[1]] = 'H';
+                //head[0] -= 1;
+                bodyParts.unshift([head[0] - 1, head[1]]);
             }
             if (this.state.direction === 'D') {
-                head[0] += 1;
-                dataCopy[head[0]][head[1]] = 'H';
+                //head[0] += 1;
+                bodyParts.unshift([head[0] + 1, head[1]]);
             }
             if (this.state.direction === 'L') {
-                head[1] -= 1;
-                dataCopy[head[0]][head[1]] = 'H';
+                bodyParts.unshift([head[0], head[1] - 1]);
+                //head[1] -= 1;
             }
             if (this.state.direction === 'R') {
-                head[1] += 1;
-                dataCopy[head[0]][head[1]] = 'H';
+                bodyParts.unshift([head[0], head[1] + 1]);
+                //head[1] += 1;
             }
+            bodyParts.pop();
 
+            //lag issues - possibly replace with my own data structure - deque
+            //write iterator and everything
 
+            //move the tail - move each part to the one before it? aka pop the last one, add new one in front?
 
             this.setState({
-                data: dataCopy,
-                head: head
+                bodyParts: bodyParts
             })
+        }
+    }
+
+    handleInteractions() {
+        const bodyParts = this.state.bodyParts.slice();
+        const head = bodyParts[0];
+        const appleLocation = this.state.appleLocation;
+
+        if (head[0] === appleLocation[0] && head[1] === appleLocation[1]) {
+            this.setState({appleLocation: null});
+            this.generateApple();
+            this.forceUpdate(); //kind of hacky
+            //add a tail part
         }
     }
 
     componentDidMount() {
         document.addEventListener("keydown", this.handleKeyDown);
-        //var intervalId = setInterval(this.timer, 1000);
     }
 
     render() {
@@ -230,6 +224,7 @@ class Game extends Component {
                     width={this.props.width}
                     height={this.props.height}
                     bodyParts={this.state.bodyParts}
+                    appleLocation={this.state.appleLocation}
                 />
             </div>
         );
--- snake/src/index.js
+++ snake/src/index.js
@@ -4,4 +4,5 @@ import './index.css';
 import Game from './Game';
 
 //600 by 400?
-ReactDOM.render(<Game width={60} height={40}/>, document.getElementById('root'));
+//ReactDOM.render(<Game width={60} height={40}/>, document.getElementById('root'));
+ReactDOM.render(<Game width={20} height={10}/>, document.getElementById('root'));
